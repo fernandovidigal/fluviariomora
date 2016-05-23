8a17cdc4ada0d63d66629b04915d3c47a61907a8
Fernando Vidigal <fernandovidigal@gmail.com>
2016-05-23 17:27:18 +0100

    Startup
    Inicio do desenvolvimento

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Fluviário de Mora</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Fluviário de Mora">
+    <meta name="author" content="Fernando Vidigal">
+    
+    <!-- FONTS -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <link rel="stylesheet" href="css/master.css">
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+   
+   <header>
+        <div class="logotipo">
+           <img src="imagens/logotipo.gif" alt="Logotipo do Fluviário">
+        </div>
+       
+        <nav>
+            <ul>
+                <li>Fluviário
+                    <ul>
+                        <li><a href="#">Missão</a></li>
+                        <li><a href="#">Conservação</a></li>
+                        <li><a href="#">Passaporte Amigo</a></li>
+                        <li><a href="#">Área Envolvente</a></li>
+                        <li class="no-bottom-border"><a href="#">Contactos</a></li>
+                    </ul>
+                </li>
+                <li>Exposição
+                   <ul>
+                        <li><a href="#">Mapa da Exposição</a></li>
+                        <li><a href="#">Percurso de um Rio</a></li>
+                        <li><a href="#">Monstros do Rio</a></li>
+                        <li><a href="#">Lontrário</a></li>
+                        <li><a href="#">Sala Saramugo</a></li>
+                        <li><a href="#">Habitats Exóticos</a></li>
+                        <li class="no-bottom-border"><a href="#">Exposição Multimédia</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Loja</a></li>
+                <li><a href="#">Restaurante</a></li>
+            </ul>
+        </nav>
+       
+   </header>
+   
+   <div id="slider_wrapper">
+        <div id="slider_images">
+            <img src="imagens/habitats_exoticos.jpg" alt="Habitats Exóticos">
+            <img src="imagens/lontrario.jpg" alt="Lontrário">
+            <img src="imagens/percurso_rio.jpg" alt="Percurso de um Rio">
+            <img src="imagens/sala_saramugo.jpg" alt="Sala Saramugo">
+        </div>
+        
+        <ul>
+            <li><a href="#">Bilheteira</a></li>
+            <li><a href="#">Horário</a></li>
+            <li><a href="#">Actividades</a></li>
+            <li><a href="#">Como Chegar</a></li>
+        </ul>
+        
+        <div class="image_bullets"></div>
+    </div>
+    
+</body>
+</html>
