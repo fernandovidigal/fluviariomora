db36ba0fa0985c778164356e3c045082b421ee91
Fernando Vidigal <fernandovidigal@gmail.com>
2016-05-31 11:10:19 +0100

    Epécies
    Página das espécies

--- percursoRio.php
+++ percursoRio.php
@@ -14,11 +14,22 @@
     
     <div class="especie">
         <img src="imagens/exposicao/bordalo.jpg" alt="Bordalo">
-        <div class="espcie-desc">
+        <div class="especie-desc">
             <h1>Bordalo</h1>
             <p><small>Iberocypris alburnoides (Steindachner, 1866)</small></p>
             <p><strong>Estatuto de Conservação:</strong> Vulnerável</p>
             <p>Espécie endémica da Península Ibérica, com distribuição generalizada na zona meridional. Em Portugal encontra-se presente nas bacias higrográficas dos rios Douro, Vouga, Mondego, Tejo, Sado, Mira e Guadiana (verificar odiel e guadalquivir). O bordalo apresenta preferência por cursos de água doce de regime lótico e não poluídos. Alguns machos e fêmeas apresentam segregação espacial de acordo com características de corrente, profundidade, substrato e coberto vegetal. Esta espécie alimenta-se principalmente de insectos aquáticos (insectívora), ingerindo também matéria vegetal. A reprodução desta espécie ovípara ocorre de Março a Julho, e realiza as posturas em zonas de cascalho com corrente. São geralmente as fêmeas que atingem maiores dimensões, cerca de 13 cm.</p>
+        </div>
+        
+    </div>
+    
+    <div class="especie">
+        <img src="imagens/exposicao/cumba.jpg" alt="Cumba">
+        <div class="especie-desc">
+            <h1>Cumba</h1>
+            <p><small>Luciobarbus Comizo (Steindachner, 1864)</small></p>
+            <p><strong>Estatuto de Conservação:</strong> Global – Vulnerável; Nacional – Em Perigo</p>
+            <p>É um endemismo ibérico, e em Portugal encontra-se nas bacias hidrográficas dos rios Tejo e Guadiana. Esta espécie ocorre em rios de grandes dimensões, de regime lótico e caudalosos mesmo durante o Verão, com abundância de plantas aquáticas. Os juvenis preferem locais mais a montante e de profundidade mais reduzida, ao contrário dos adultos que preferem zonas mais profundas e a jusante. O cumba alimenta-se de plâncton, insectos aquáticos, outros peixes e ocasionalmente de matéria vegetal e detritos. A reprodução desta espécie ovípara ocorre de Maio a Junho, em zonas profundas de corrente lenta e substrato de cascalho. É a espécie de barbo que atinge maiores dimensões em território nacional, cerca de 100 cm.</p>
         </div>
         
     </div>
